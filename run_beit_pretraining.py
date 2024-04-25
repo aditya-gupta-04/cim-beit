fbb8df946fccbf7cf325437545f8c35a6429aa85
aditya-gupta-04 <106423963+aditya-gupta-04@users.noreply.github.com>
2024-04-25 16:38:33 +0530

    Update run_beit_pretraining.py

--- run_beit_pretraining.py
+++ run_beit_pretraining.py
@@ -129,6 +129,8 @@ def get_args():
     parser.add_argument('--dist_on_itp', action='store_true')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
 
+    parser.add_argument('--distributed', default=False)
+
     return parser.parse_args()
 
 
